85825a41359d539b7bcaee290d73b746732ce6b0
Elena Piaggio <elenapiaggio@gmail.com>
2020-09-28 09:36:00 +0200

    initial commit

---
+++ src/main/java/com/itacademy/billionaires/BillionairesApplication.java
@@ -0,0 +1,13 @@
+package com.itacademy.billionaires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillionairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillionairesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itacademy/billionaires/BillionairesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itacademy.billionaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillionairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
